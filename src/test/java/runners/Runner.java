1e6e20990931734216b86b11cace3df482113133
yasinaniltechpro <yasinanil.techproed@gmail.com>
2023-06-05 22:21:40 +0300

    1

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features="src/test/resources/endtoend_features",
+        glue= "stepdefinitions",
+        tags= "@e2e",//Çalışacak scenarioları belirtir.
+        dryRun= false
+)
+
+public class Runner {
+}
